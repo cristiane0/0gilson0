a162eb93f570b9702d3b87baa5fc871c895ec843
cristiane0 <138214414+cristiane0@users.noreply.github.com>
2024-10-16 08:44:38 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Historía',
+    'Onde surgiram os primeios ancestrais do homem ?',
+    'África Oriental'
+)
+
+criaCartao(
+    'Países ',
+    'Qual país e considerado o mais rico?',
+    'O país considerado rico e o Luxemburgo '
+)
+
+criaCartao(
+    'Programação',
+    'Qual a função do html?',
+    'código que você usa para estruturar uma página web e seu conteúdo'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz camaleão em ingles?',
+    'camaleão em ingles se diz chameleon'
+)
